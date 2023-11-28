1a73e020d533992ac0d240b4d70a73d20b2820f6
Rhys Mills <rhys.mills@guardian.co.uk>
2023-11-28 13:50:42 +0000

    Represent utf-8 text as notes in two pitches

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
-
+from scipy.io import wavfile 
+import bitarray
 import numpy as np
 import pyaudio
 import functools 
 
+
 p = pyaudio.PyAudio()
 
-volume = 0.5  # range [0.0, 1.0]
+testString = "av a washi"
+volume = 0.01  # range [0.0, 1.0]
 fs = 44100  # sampling rate, Hz, must be integer
 duration = 0.1  # in seconds, may be float
 f = 300.0  # sine frequency, Hz, may be float
@@ -18,12 +21,38 @@ def freqToNote(freq: int):
         2 * np.pi * np.arange(fs * duration) * freq / fs, 
     )).astype(np.float32)
 
-freqs_as_notes = map(lambda a: (volume * freqToNote(a)).tobytes(), notes)
+# def concat(a: np.ndarray[np.float32] ,b: np.ndarray[np.float32]):
+#     functools.reduce(lambda a,b: np.concatenate((a,b)), freqs_as_notes) 
 
+def stringToInts(string: str):
+    ba = bitarray.bitarray()
+    ba.frombytes(string.encode('utf-8'))
+    l = ba.tolist()
+    notes: list[int] = []
+    for i in range(0, len(l)):
+        notes.append(500 if l[i] else 2700)
+    return notes  
+
+def intsToNotes(ints: list[int]):
+    freqs_as_notes = map(lambda a: (volume * freqToNote(a)), ints)
+    return functools.reduce(lambda a,b: np.concatenate((a,b)), freqs_as_notes)
+
+def stringToNotes(str: str):
+    return intsToNotes(stringToInts(str))
+
+
+freqs_as_notes = map(lambda a: (volume * freqToNote(a)), notes)
+# freqs_as_notes_reversed = map(lambda a: (volume * freqToNote(a)), reversed(notes))
+
+notes = functools.reduce(lambda a,b: np.concatenate((a,b)), freqs_as_notes)
+# notes_reversed = functools.reduce(lambda a,b: np.concatenate((a,b)), freqs_as_notes_reversed)
+
+# concat = np.concatenate((notes, notes_reversed, notes, notes_reversed))
 # generate samples, note conversion to float32 array
-output_bytes = functools.reduce(lambda a,b: a+b, freqs_as_notes)
+# output_bytes = functools.reduce(lambda a,b: a+b, freqs_as_notes)
+# output_bytes_reversed = functools.reduce(lambda a,b: a+b, freqs_as_notes_reversed)
 
-print(output_bytes)
+# all_bytes = output_bytes + output_bytes_reversed + output_bytes + output_bytes_reversed
 
 # per @yahweh comment explicitly convert to bytes sequence
 # output_bytes = (volume * samples).tobytes() + (volume * sample2).tobytes()
@@ -35,11 +64,17 @@ stream = p.open(format=pyaudio.paFloat32,
                 output=True)
 
 # play. May repeat with different volume values (if done interactively)
-start_time = time.time()
-stream.write(output_bytes + output_bytes)
-print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+# start_time = time.time()
+# stream.write(all_bytes)
+# print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+# stream.stop_stream()
+# stream.close()
 
-stream.stop_stream()
-stream.close()
+our_notes = stringToNotes(testString)
+print(notes)
+print(our_notes)
+wavfile.write('abc1.wav', fs, our_notes)
+
+
 
 p.terminate()
